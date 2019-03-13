74afe2be940c086d0da816ff0855ddcd83ebfa99
Sun1ive <sunliveua@gmail.com>
2019-03-13 20:04:26 +0200

    upd

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { renderFile } from 'ejs';
 import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useStaticAssets(join(__dirname, '../../client/build'));
+  app
+    .engine('html', renderFile)
+    .setBaseViewsDir(join(__dirname, '../../client/build'));
+
+  app.useStaticAssets(join(__dirname, '../../client/build'), {
+    index: false,
+    redirect: false,
+  });
+  // app.setBaseViewsDir(join(__dirname, '../../client/build'));
 
   await app.listen(3000);
 }
