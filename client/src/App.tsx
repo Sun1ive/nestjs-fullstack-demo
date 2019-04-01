2902a422cedb1363a28f04fdcacd4b6d3ab9e6be
Sun1ive <sunliveua@gmail.com>
2019-04-01 14:14:58 +0300

    upd client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  public componentDidMount() {
+    const ws = connect('http://localhost:3000');
+
+    ws.on('connection', () => {
+      console.log('Connected');
+    });
+
+    ws.on('error', (err: Error) => {
+      console.log('Error while connecting %o', err);
+    });
+
+    ws.on('hello', (data: any) => {
+      console.log('New data incoming %o', data);
+    });
+  }
+
   render() {
     return (
       <div className="App">
